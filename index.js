419585ef7209feb80ae6d9cd1a9f428c903fd071
Julius Magomere <makojulius@gmail.com>
2023-11-17 15:34:35 +0300

    added try...catch blocks around the code sections that might potentially throw errors.

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ let selectedRating = "";
 
 ratingEls.forEach((ratingEl) => {
   ratingEl.addEventListener("click", (event) => {
+    try {
+
     removeActive();
     selectedRating =
       event.target.innerText || event.target.parentNode.innerText;
     event.target.classList.add("active");
     event.target.parentNode.classList.add("active");
+  } catch (error) {
+    console.log('Error occurred,Check Connection:', error); 
+   }
   });
 });
 
 btnEl.addEventListener("click", () => {
+  try {
+    
   if (selectedRating !== "") {
     containerEl.innerHTML = `
         <strong>Hujambo,Thank you for the feedback!</strong>
@@ -27,10 +34,21 @@ btnEl.addEventListener("click", () => {
         <p>Your feedback will be used to improve our customer support.</p>
         `;
   }
+} catch (error) {
+    console.log('Error occurred,Check Connection:', error); 
+  
+}
 });
 
 function removeActive() {
+  try {
+  
   ratingEls.forEach((ratingEl) => {
     ratingEl.classList.remove("active");
   });
+} catch (error) {
+  console.log('Error occurred,Check Connection:', error); 
 }
+}
+
+
