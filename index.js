ac8989d8a78f1526a68891ded7f402fca4e7799d
Julius Magomere <makojulius@gmail.com>
2023-11-22 17:38:24 +0300

    adding a fallback incase male voice is not available result to default

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeActive() {
 } catch (error) {
   console.log('Error occurred,Check your Connection:', error); 
 }
-}
+}g
 
 function readResponseAloud(text) {
   const speech = new SpeechSynthesisUtterance();
